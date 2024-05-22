37805afb504362681abafe7df0aed024af8a9053
m43i <36700796+m43i@users.noreply.github.com>
2024-05-22 20:25:44 +0200

    fix: awaits

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,20 @@ use tokio::{sync::mpsc, time::sleep};
 
 mod proto;
 
+struct ResponseData {}
+
 #[tokio::main]
 async fn main() {
-    let(tx, mut rx) = mpsc::channel(36);
+    let(tx, mut rx) = mpsc::channel::<ResponseData>(36);
 
     tokio::spawn(async move {
         loop {
             // request here
             
-            sleep(Duration::from_secs(15))
+            sleep(Duration::from_secs(15)).await
         }
     });
 
-    while let Some(data) = rx.recv() {
-        // process data
+    while let Some(data) = rx.recv().await {
     }
 }
