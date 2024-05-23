b07e11d3b8d68689d91a8417f16d9143d9a5c31f
m43i <36700796+m43i@users.noreply.github.com>
2024-05-23 18:27:00 +0200

    feat: add linux aarch 64

--- protoc-install.sh
+++ protoc-install.sh
@@ -6,12 +6,26 @@ PB_REL="https://github.com/protocolbuffers/protobuf/releases"
 
 PROTOC_PATH="$INS_PATH"/bin/protoc
 
+# Get system architecture
+ARCH=$(uname -m)
+
 # if protoc path not exists
 if [ ! -f "$PROTOC_PATH" ]; then
     echo "PROTOC not found, installing..."
+
     mkdir -p "$INS_PATH"
-    # Download the binary
-    wget -O "$INS_PATH"/protoc.zip $PB_REL/download/v26.1/protoc-26.1-linux-x86_64.zip
+
+    if [ "$ARCH" = "x86_64" ]; then
+        # Download x86_64 binary
+        wget -O "$INS_PATH"/protoc.zip $PB_REL/download/v27.0/protoc-27.0-linux-x86_64.zip
+    elif [ "$ARCH" = "aarch64" ]; then
+        # Download aarch64 binary
+        wget -O "$INS_PATH"/protoc.zip $PB_REL/download/v27.0/protoc-27.0-linux-aarch_64.zip
+    else
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+    fi
+
     # Unzip the binary
     unzip "$INS_PATH"/protoc.zip -d "$INS_PATH"
 
@@ -19,3 +33,4 @@ if [ ! -f "$PROTOC_PATH" ]; then
     rm -f "$INS_PATH"/protoc.zip
     rm -f "$INS_PATH"/readme.txt
 fi
+
